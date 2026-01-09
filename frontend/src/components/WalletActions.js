fa170bab12b4280bb3d63d4667b806537a3411bf
Asafe <contatoasapdev@gmail.com>
2026-01-09 14:57:25 -0300

    Limpeza completa: remover arquivos antigos, stubs e temporários

--- frontend/src/components/WalletActions.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function WalletActions() {
-  return <div>WalletActions</div>;
-}
--- frontend/src/components/WalletChart.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function WalletChart() {
-  return <div>WalletChart</div>;
-}
